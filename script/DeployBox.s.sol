eedf7972e77e0ca4498e27ddd4d2459d5b974fa4
Olasunkanmi <olasunkanmiibalogun@gmail.com>
2024-06-22 11:40:11 +0100

    Clear Git Cache

--- script/DeployBox.s.sol
+++ script/DeployBox.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.18;
 
 import { Script } from "forge-std/Script.sol";
 import { BoxV1 } from "../src/BoxV1.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol"; // Proxy type we're using
 
 contract DeployBox is Script {
 
@@ -12,10 +13,10 @@ contract DeployBox is Script {
         return proxy;
     }
 
-    function deployBox() public {
-        BoxV1 box = new BoxV1();
-        
-
+    function deployBox() public returns (address) {
+        BoxV1 box = new BoxV1(); //here's the implementation
+        ERC1967Proxy proxy = new ERC1967Proxy(address(box), ""); //passing the implementation to the proxy
+        return address(proxy);  
     }
 
 }
--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -2,3 +2,10 @@
 
 pragma solidity ^0.8.18;
 
+import { Script } from "forge-std/Script.sol";
+import { BoxV2 } from "../src/BoxV2.sol";
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol"; // Proxy type we're using
+
+contract UpgradeBox is Script {
+   
+}
